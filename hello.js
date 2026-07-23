66d2817004a9b53d66c16b48e1da46abb25536fe
Alpha <kennethprecious117@gmail.com>
2026-07-23 16:52:35 +0100

    Create hello.js
    Testing my first PR

---
+++ hello.js
@@ -0,0 +1,7 @@
+// A simple test function to try out our PR + AI review workflow
+
+function greetUser(name) {
+  console.log("Hello, " + name + "! Welcome to the project.");
+}
+
+greetUser("World");
